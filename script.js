5bdcab55e37c7ce069d120cd8ec1c4ba45050717
MoMoktan <112465095+MoMoktan@users.noreply.github.com>
2024-03-12 14:19:01 +0545

    file has been updated

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
-
-
-
 const firstName = document.getElementById('firstname');
 const lastName = document.getElementById('lastname');
-const address = document.getElementById('address')
-const mail = document.getElementById('email')
-const num = document.getElementById('number')
+const address = document.getElementById('address');
+const mail = document.getElementById('email');
+const num = document.getElementById('number');
 
 function validation (){
     
     if(firstName.value == ''){
- return false;
-    } else if (lastName.value =''){
-      return  false
-const address = document.getElementById('address')
-    }else if (address.value == ''){
- return false;
-
-    } else if (mail.value == ''){
-        return false;
-    }else if (number.value == ''){
-        return false;
-    }
-    else{
-        return true;
+        firstName.classList.add('error');
+        return 'Please Enter Your First Name';
+    } else {
+        firstName.classList.remove('error');
     }
 
+    if (lastName.value == ''){
+        lastName.classList.add('error');
+        return 'Please Enter Your Last Name';
+    } else {
+        lastName.classList.remove('error');
+    }
+
+    if (address.value == ''){
+        address.classList.add('error');
+        return 'Input The Address to Continue';
+    } else {
+        address.classList.remove('error');
+    }
+
+    if (mail.value == ''){
+        mail.classList.add('error');
+        return 'Enter Mail Id';
+    } else {
+        mail.classList.remove('error');
+    }
+
+    if (num.value == ''){
+        num.classList.add('error');
+        return 'Please Provide Your Number';
+    } else {
+        num.classList.remove('error');
+    }
+
+    return true;
 }
+
 const submitBtn = document.getElementById('submit');
 
 submitBtn.addEventListener('click',(e)=>{
-   if(! validation){
-    e.preventDefault()
-    console.log('fill all the details')
-   } else{
-    console.log("form submitted")
-}
- 
-
-})
+   const validationResult = validation();
+   if(validationResult !== true){
+        e.preventDefault();
+        console.log(validationResult);
+   } else {
+        console.log("form submitted");
+   }
+});
