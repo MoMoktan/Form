0132f79fd6bdda8a8ea1b72a87a4158daf8ef537
MoMoktan <112465095+MoMoktan@users.noreply.github.com>
2024-03-12 10:58:31 +0545

    file updated

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+
+
+
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const address = document.getElementById('address')
+const mail = document.getElementById('email')
+const num = document.getElementById('number')
+
+function validation (){
+    
+    if(firstName.value == ''){
+ return false;
+    } else if (lastName.value =''){
+      return  false
+const address = document.getElementById('address')
+    }else if (address.value == ''){
+ return false;
+
+    } else if (mail.value == ''){
+        return false;
+    }else if (number.value == ''){
+        return false;
+    }
+    else{
+        return true;
+    }
+
+}
 const submitBtn = document.getElementById('submit');
 
-submitBtn.addEventListener(click,()=>{
- console.log("form submitted")
+submitBtn.addEventListener('click',(e)=>{
+   if(! validation){
+    e.preventDefault()
+    console.log('fill all the details')
+   } else{
+    console.log("form submitted")
+}
+ 
 
 })
